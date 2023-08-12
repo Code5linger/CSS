fb80b93d145b1c1d89a62f56aea378b427fef813
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2023-08-12 17:39:45 +0600

    !

---
+++ Kevin Powell - CSS Demystified v3/MISC/elemts/script.js
@@ -0,0 +1,18 @@
+function consolePrint() {
+  console.log("Yo!");
+}
+
+function alertPrompt() {
+  alert("Yo");
+}
+
+function domManipulation() {
+  //   document.getElementById("demo").innerHTML = Date();
+  document.getElementById("demo").innerHTML = Date();
+}
+
+function showDate() {
+  document.getElementById("demo").innerHTML = Date();
+  console.log("Yo!");
+  alert("Yo");
+}
